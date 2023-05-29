6f71133869c95eb9daff044fedf4af0153d48230
Alaa-M1 <alaa85a@gmail.com>
2023-05-29 18:04:47 +0200

    move Firebase configuration parameters to .env file

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithRedirect, createUserWithEmailAndPassword, UserCredential, signInWithEmailAndPassword, FacebookAuthProvider } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
-
+console.log('process.env.API_KEY=',process.env.REACT_APP_API_KEY)
 const firebaseConfig = {
-  apiKey: "AIzaSyB5NpEnX4xF1toHctG3qtzdvSIu8Pbn_So",
-  authDomain: "ecommerceproject-abb33.firebaseapp.com",
-  projectId: "ecommerceproject-abb33",
-  storageBucket: "ecommerceproject-abb33.appspot.com",
-  messagingSenderId: "607618773174",
-  appId: "1:607618773174:web:202e0756957f99b826b617",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
