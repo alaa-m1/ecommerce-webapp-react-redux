0ac7145ac3ca300c3b224abf50f4a923ff3a40b2
Alaa-M1 <alaa85a@gmail.com>
2023-06-05 18:40:59 +0200

    Add persist to shoppingCart

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "store/store";
+import { persistor, store } from "store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,23 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 import { rootReducer } from './rootReducer';
+import storage from 'redux-persist/es/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: ['user','categories']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 //The dispatch action will trigger the middleware before the the action trigger the reducer
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
