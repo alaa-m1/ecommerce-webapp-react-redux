b7779186575b87356d0b3f99520b14512088eabc
Alaa Mohammad <alaa85a@gmail.com>
2023-06-18 16:44:05 +0300

    update reduc => categories

--- src/store/categories/categoriesActions.ts
+++ src/store/categories/categoriesActions.ts
@@ -1,27 +1,27 @@
-import { createAction } from "utils/redux/reduxUtils";
+import { Action, ActionWithPayload, createAction } from "utils/redux/reduxUtils";
 import { categoriesActionTypes } from "./categoriesActionTypes";
 import { CategoriesResponse } from "types";
-import { Action, AnyAction, Dispatch } from "redux";
 import { categoriesRes } from "shared";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
-import { CategoryState } from "./categoriesReducer";
+import { ThunkAction } from "redux-thunk";
 import { RootState } from "store/store";
 
+type FetchCategoriesStart = Action<categoriesActionTypes.FETCHING_CATEGORIES_START>
+type FetchCategoriesFailed = ActionWithPayload<categoriesActionTypes.FETCHING_CATEGORIES_FAILED, Error>
+type FetchCategoriesSuccess = ActionWithPayload<categoriesActionTypes.FETCHING_CATEGORIES_SUCCESS, CategoriesResponse>
 
-const fetchCategoriesStart = () => createAction(categoriesActionTypes.FETCHING_CATEGORIES_START);
-const fetchCategoriesFailed = (error: any) => createAction(categoriesActionTypes.FETCHING_CATEGORIES_FAILED, error);
-const fetchCategoriesSuccess = (categories: CategoriesResponse) => createAction(categoriesActionTypes.FETCHING_CATEGORIES_SUCCESS, categories);
+export type CategoriesAction = FetchCategoriesStart | FetchCategoriesFailed | FetchCategoriesSuccess;
 
+const fetchCategoriesStart = (): FetchCategoriesStart => createAction(categoriesActionTypes.FETCHING_CATEGORIES_START);
+const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed => createAction(categoriesActionTypes.FETCHING_CATEGORIES_FAILED, error);
+const fetchCategoriesSuccess = (categories: CategoriesResponse): FetchCategoriesSuccess => createAction(categoriesActionTypes.FETCHING_CATEGORIES_SUCCESS, categories);
 
-export const fetchCategoriesAsync = ():ThunkAction<void, RootState, unknown, any> => async (dispatch) => {
+export const fetchCategoriesAsync = (): ThunkAction<void, RootState, unknown, any> => async (dispatch) => {
     try {
         dispatch(fetchCategoriesStart());
-        const categories =  categoriesRes; // here we should fetch the categories from the server
+        const categories = categoriesRes; // here we should fetch the categories from the server
         return dispatch(fetchCategoriesSuccess(categories))
-        console.log('FETCHING_CATEGORIES_SUCCESS fetchhhh===',categories)
 
     } catch (error: any) {
-        console.log('FETCHING_CATEGORIES_SUCCESS error===',error)
         return dispatch(fetchCategoriesFailed(error))
 
     }
