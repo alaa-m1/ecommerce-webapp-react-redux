e1706ef72b94e1af129a996dceb1e39b02664241
Alaa Mohammad <alaa85a@gmail.com>
2023-09-24 21:20:04 +0300

    Add the LoadingSpinner to the Home page

--- src/pages/Home/HomeDashboard.tsx
+++ src/pages/Home/HomeDashboard.tsx
@@ -8,25 +8,32 @@ import { useProducts } from "pages/OnlineShop/hooks";
 import { setProducts } from "store/products/productsActions";
 import { useDispatch } from "react-redux";
 import _ from "lodash";
-import { selectMappedProducts } from "store/products/productsSelector";
+import { selectMappedProducts, selectProductsStatus } from "store/products/productsSelector";
 
 const HomeDashboard = () => {
   const dispatch = useDispatch();
   /// Using react-query to manage request state with caching (The other good solution to use with Redux is RTK Query)
-  const { data } = useProducts(100);
+  const { data, isLoading } = useProducts(100);
   useEffect(() => {
     if (!_.isUndefined(data)) dispatch(setProducts(data));
   }, [data]);
 
-  /// Using Redux to manage request status (Comment the last two commands and then uncomment the next command)
+  /// Using Redux to manage request status (Comment the last two commands and then uncomment the next two commands)
   // useEffect(() => {
   //   dispatch(fetchProductsAsync() as any);
   // }, [dispatch]);
+  // const isLoading=false;
 
   const onlineCategories = useAppSelector(selectMappedProducts);
+  const status = useAppSelector(selectProductsStatus);
+
+  const loading = useMemo(
+    () => isLoading || status.loading,
+    [isLoading, status.loading]
+  );
+
   const categories1 = useAppSelector(selectMappedCategories)
   const allCategories = useMemo(() => [...onlineCategories, ...categories1], [categories1, onlineCategories])
-  const { loading, error } = useAppSelector(selectCategoriesStatus);
   const mainCategoriesLabels = useMemo(
     () =>
       allCategories.reduce<Array<string>>((res, category) => {
