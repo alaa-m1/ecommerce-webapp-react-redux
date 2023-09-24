bd434133a12aec2d0e0fdcbfb9e08d984e2a3814
Alaa Mohammad <alaa85a@gmail.com>
2023-09-24 21:01:05 +0300

    Add Footer, Update cart-scroller and cart-card style

---
+++ src/pages/AppLayout/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material"
+import { Link } from "react-router-dom"
+import EmailIcon from '@mui/icons-material/Email';
+
+export const Footer = () => {
+    return (
+        <Box className="footer-section">
+            <Box>
+                <Link to={"/"}>
+                    Home
+                </Link>
+            </Box>
+            <Box sx={{display:"flex"}}><EmailIcon/> <a href="mailto:alaa85a@gmail.com">alaa85a@gmail.com</a></Box>
+        </Box>
+    )
+}
