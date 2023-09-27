b2f04d479cd5a42a0d0e985fc9c1bdc966741d8d
Alaa Mohammad <alaa85a@gmail.com>
2023-09-27 17:08:10 +0300

    Remove unused imports

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, compose, applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore, compose, applyMiddleware } from "@reduxjs/toolkit";
 import {
   persistStore,
   persistReducer,
@@ -10,37 +10,42 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import logger from 'redux-logger';
+import logger from "redux-logger";
 
-import { rootReducer } from './rootReducer';
-import thunkMiddleware from 'redux-thunk'
+import { rootReducer } from "./rootReducer";
+import thunkMiddleware from "redux-thunk";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ['shoppingCart']
+  whitelist: ["shoppingCart"],
 };
-
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = process.env.MODE_ENV === 'development' ? [logger,thunkMiddleware] : [thunkMiddleware];
-const middlewareEnhancer = applyMiddleware(...middlewares)
+const middlewares =
+  process.env.MODE_ENV === "development"
+    ? [logger, thunkMiddleware]
+    : [thunkMiddleware];
+const middlewareEnhancer = applyMiddleware(...middlewares);
 
-const enhancers = [middlewareEnhancer]
-const composedEnhancers = (process.env.MODE_ENV !== 'production' && typeof window !== 'undefined' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose(...enhancers);
+const enhancers = [middlewareEnhancer];
+const composedEnhancers =
+  (process.env.MODE_ENV !== "production" &&
+    typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose(...enhancers);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware:
-    (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        enhancers: composedEnhancers,
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      })
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      enhancers: composedEnhancers,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export let persistor = persistStore(store);
 export default store;
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
