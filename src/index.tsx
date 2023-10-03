40ac749e7e8128fc7788b10207e692e8652fb8d0
Alaa Mohammad <alaa85a@gmail.com>
2023-10-03 13:34:59 +0300

    Add basic PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ import "./i18n";
 import ThemedApp from "ThemedApp";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripeOptions, stripePromise } from "utils/stripe/stripe";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -25,6 +27,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
