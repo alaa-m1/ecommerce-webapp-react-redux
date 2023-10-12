10f25a1daaf5ffb120373ffbf08fb9541d82dec4
Alaa Mohammad <alaa85a@gmail.com>
2023-10-12 17:27:18 +0300

    Update Cypress config

--- cypress/constants/index.js
+++ cypress/constants/index.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:3005";
+export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "https://phoenix-ecommerce.netlify.app/";
 
 /*
  *loginFakeResponse and lookupFakeResponse are using to
