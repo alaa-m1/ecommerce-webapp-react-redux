63758ccd23364d827019fd7f14a250885977e91e
Alaa Mohammad <alaa85a@gmail.com>
2023-12-07 12:14:00 +0300

    Fix Netlify Pipeline error

--- src/utils/actions/productActions.ts
+++ src/utils/actions/productActions.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "utils/axiosInstance";
 
-export const getProducts = async (limit: number = 100) => {
+export const getProducts = async (limit = 100) => {
   try {
     const response = await axiosInstance.get(`products?_limit=${limit}`);
     return response.data;
