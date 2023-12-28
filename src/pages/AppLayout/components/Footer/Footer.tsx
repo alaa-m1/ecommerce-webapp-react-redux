d4fdf3b6a27697920e30c76c6dfa15eedd5e2fff
Alaa Mohammad <alaa85a@gmail.com>
2023-12-28 12:14:56 +0300

    Update Footer style

--- src/pages/AppLayout/components/Footer/Footer.tsx
+++ src/pages/AppLayout/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import EmailIcon from "@mui/icons-material/Email";
 
@@ -9,20 +9,31 @@ export const Footer = () => {
       container
       className="footer-section"
       sx={{ backgroundColor: "secondary.light" }}
+      alignItems="center"
     >
-      <Grid item sm={6} xs={12}>
+      <Grid item sm={6} xs={12} textAlign="center">
         <Link to={"/"}>
           <Typography color="primary.main">E-commerce</Typography>
         </Link>
       </Grid>
-      <Grid item sm={6} xs={12} sx={{ display: "flex" }}>
-        <EmailIcon sx={{ "& path": { color: "primary.main" } }} />
+      <Grid
+        item
+        sm={6}
+        xs={12}
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        <Box sx={{ display: "flex" }}>
+          <EmailIcon sx={{ "& path": { color: "primary.main" } }} />
 
-        <a href="mailto:alaa85a@gmail.com">
-          <Typography color="primary.main" style={{ textOverflow: "ellipsis" }}>
-            alaa85a@gmail.com
-          </Typography>
-        </a>
+          <a href="mailto:alaa85a@gmail.com">
+            <Typography
+              color="primary.main"
+              style={{ textOverflow: "ellipsis" }}
+            >
+              alaa85a@gmail.com
+            </Typography>
+          </a>
+        </Box>
       </Grid>
     </Grid>
   );
