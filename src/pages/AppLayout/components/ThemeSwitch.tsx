38886284a835b8f73490903ed13ed4d220057da4
Alaa Mohammad <alaa85a@gmail.com>
2023-12-29 16:40:45 +0300

    Replace fixed colors with colors from MUI theme

--- src/pages/AppLayout/components/ThemeSwitch.tsx
+++ src/pages/AppLayout/components/ThemeSwitch.tsx
@@ -5,15 +5,17 @@ import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "utils/redux/hooks";
 import { setCurrentThemeMode } from "store/user/userActions";
+import { useTheme } from "@mui/material";
 
 export const ThemeSwitch = () => {
   const currentThemeMode = useAppSelector((state) => state.user.themeMode);
   const dispatch = useDispatch();
+  const theme= useTheme();
   return (
     <IconButton
       sx={{
         ml: 1,
-        "& path": { color: currentThemeMode === "dark" ? "#ff9800" : "#000" },
+        "& path": { color: currentThemeMode === "dark" ? theme.palette.secondary.main : theme.palette.primary.light },
       }}
       onClick={() =>
         dispatch(
--- src/pages/Home/components/InfoSection.tsx
+++ src/pages/Home/components/InfoSection.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Alert, Box } from "@mui/material";
+import { Alert, Box, useTheme } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useAppSelector } from "utils/redux/hooks";
 
 export const InfoSection = () => {
   const { t } = useTranslation();
+  const theme= useTheme();
   const currentThemeMode = useAppSelector((state) => state.user.themeMode);
   return (
     <Box
@@ -18,7 +19,7 @@ export const InfoSection = () => {
         sx={{ mb: 1, width: "60%" }}
         severity="info"
         style={{
-          backgroundColor: currentThemeMode === "dark" ? "#e65100" : "#0288d1",
+          backgroundColor: currentThemeMode === "dark" ?  theme.palette.secondary.main : theme.palette.secondary.main,
         }}
       >
         {t("home_api_info")}:&nbsp;
@@ -28,7 +29,7 @@ export const InfoSection = () => {
         sx={{ mb: 1, width: "60%" }}
         severity="info"
         style={{
-          backgroundColor: currentThemeMode === "dark" ? "#e65100" : "#0288d1",
+          backgroundColor: currentThemeMode === "dark" ? theme.palette.secondary.main : theme.palette.secondary.main,
         }}
       >
         {t("home_translation_info")}
