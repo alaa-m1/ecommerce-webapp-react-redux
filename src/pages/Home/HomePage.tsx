6ba419bec5815a06c4145c8a13478bcd82422cf6
Alaa Mohammad <alaa85a@gmail.com>
2024-10-24 10:23:25 +0300

    feat: Add a progress bar to [AppLayout]

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { LoadingSpinner } from "shared";
 import { InfoSection, MainCategoriesList } from "./components";
 import { useCategoriesLables } from "./hooks";
-
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { Box } from "@mui/material";
 const HomePage = () => {
-
-
-  const { allCategories, mainCategoriesLabels, loading } = useCategoriesLables();
-
+  const { allCategories, mainCategoriesLabels, loading } =
+    useCategoriesLables();
+    const x = useMotionValue(0)
+    const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
+    console.log('x=',x);
   return (
     <>
       {loading && <LoadingSpinner />}
@@ -15,6 +17,19 @@ const HomePage = () => {
         mainCategoriesLabels={mainCategoriesLabels}
         categories={allCategories}
       />
+      <Box sx={{ m: "auto" }}>
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 1.1 }}
+          drag="x"
+          dragConstraints={{ left: -100, right: 100 }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          style={{ x, opacity }}
+        >
+          mmmm
+        </motion.div>
+      </Box>
       <InfoSection />
     </>
   );
