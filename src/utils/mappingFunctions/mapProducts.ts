b5eae01a0f0d3d8326b21a3385bc1da0bcee0bb5
Alaa Mohammad <alaa85a@gmail.com>
2026-04-13 23:29:59 +0200

    feat: add category filtering to exclude groceries from product mapping with case-insensitive comparison.

--- src/utils/mappingFunctions/mapProducts.ts
+++ src/utils/mappingFunctions/mapProducts.ts
@@ -2,19 +2,24 @@ import { Products, ProductsResponse } from "types";
 import _ from "lodash";
  
 export declare type MapResponseFn<TDATA, RDATA = unknown> = (res: RDATA) => TDATA;
+
+const EXCLUDED_CATEGORIES = ['groceries'];
+
 export const mapProducts: MapResponseFn<Products, ProductsResponse> = (
   res: ProductsResponse
 ): Products => {
-  const products: Products = res.products.map((product) => ({
-    id: product.id,
-    title: product.title,
-    price :product.price,
-    description: product.description,
-    categoryLabel: product.category,
-    imagePath: product.thumbnail,
-    rating: { rate: product.rating, count: product.stock },
-    uId:_.uniqueId()
-  }))
+  const products: Products = res.products
+    .filter((product) => !EXCLUDED_CATEGORIES.includes(product.category.toLowerCase()))
+    .map((product) => ({
+      id: product.id,
+      title: product.title,
+      price :product.price,
+      description: product.description,
+      categoryLabel: product.category,
+      imagePath: product.thumbnail,
+      rating: { rate: product.rating, count: product.stock },
+      uId:_.uniqueId()
+    }))
   return products
 };
 
